09658d976b767ef5eec68bc3ee1ec753f9201697
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2022-11-08 11:43:30 +0100

    New Update

---
+++ rect02.m
@@ -0,0 +1,11 @@
+%Signal rectangulaire périodique :
+% rect(t-1)
+
+t=-6:0.001:6;
+x = square(2*pi*(t+0.25)*-1);
+plot(t,x);
+axis([-8,8 -2,2]);
+grid on 
+xlabel('temps');
+ylabel('x(t)');
+title('signal rect(t)');
