7ebaed4fc2b1902d0595fee06418491ea423a012
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2022-11-11 17:41:25 +0100

    New Update

--- rect03.m
+++ rect03.m
@@ -11,6 +11,9 @@ v1= square(v); % Ce pour faire x2 = (t-1)^2
 
 x = -square(v1); % Ce represente x(t)=rect(t-1)^2 
 
+x(x<0)=0; 
+x(x>0)=1; 
+
 % plot x = rect(t-1)^2
 plot(t,v);
 axis([-8,8 -2,2]);
