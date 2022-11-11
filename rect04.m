d5972ffd2f07066d8b6ce6f83d2c0e4d15691a5c
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2022-11-11 11:48:55 +0100

    update

--- rect04.m
+++ rect04.m
@@ -7,16 +7,14 @@ clear all
 
 t=-6:0.001:6;
  
-x1 = pi*t+1.5;  % il montre  v = t-1
-x2= square(x1); % Ce pour faire x2 = (t-1)^2 
+v = pi*t+1.5;  % il montre  v = t-1
+v1= square(v); % Ce pour faire x2 = (t-1)^2 
 
-x3 = -square(x2); % Ce represente x(t)=rect(t-1)^2 
-x3(x3<0)=-1;
-x3(x3>0)=1;
+x = -square(v1); % Ce represente x(t)=rect(t-1)^2 
 
-w = x2 - x3;
-w(w<0)=-1;
-w(w>0)=1;
+
+w = v1 - x;
+
 % plot w(t) = x(t)-(x(t-1)^2)
 plot(t,w);
 axis([-8,8 -2,2]);
